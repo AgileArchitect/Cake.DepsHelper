cc40a21200b4838bdf4965b4fd5d73d76490a9ff
Tom Staijen <t.staijen@gmail.com>
2016-12-08 23:28:07 +0100

    initial

---
+++ Cake.DepsHelper.Test/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Cake.Core.IO;
+using NUnit.Framework;
+
+namespace Cake.DepsHelper.Test
+{
+    [TestFixture]
+    public class TestDepsHelper
+    {
+        [Test]
+        public void TestPackageDependencies()
+        {
+            var dir = Directory.GetParent(Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).FullName).FullName;
+
+            var packages = System.IO.Path.Combine(dir, "packages.config");
+
+            var deps = DepsHelperAliases.PackageDependencies(null, new FilePath(packages)).ToList();
+
+            Assert.IsNotEmpty(deps);
+            Assert.That(deps.Any(d => d.Name == "Cake.Core"));
+            Assert.That(deps.Any(d => d.Name == "NUnit"));
+        }
+    }
+}
---
+++ Cake.DepsHelper/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cake.Core;
+using Cake.Core.IO;
+
+namespace Cake.DepsHelper
+{
+    public static class DepsHelperAliases
+    {
+        public static IEnumerable<Dependency> PackageDependencies(this ICakeContext context, FilePath path)
+        {
+            System.Xml.XmlDocument packages = new System.Xml.XmlDocument();
+            var lines = System.IO.File.ReadAllLines(path.FullPath);
+            var content = string.Join(Environment.NewLine, lines);
+            content = content.Replace("utf-8", "utf-16");
+            packages.LoadXml(content);
+            if (packages.DocumentElement == null)
+                return new Dependency[] { };
+
+            var dependencies = packages.DocumentElement.SelectNodes("/packages/package").Cast<System.Xml.XmlNode>().Select(x => new Dependency { Name = x.Attributes["id"].Value, Version = x.Attributes["version"].Value }).ToList();
+            return dependencies;
+        }
+    }
+
+    public class Dependency
+    {
+        public string Name { get; set; }
+        public string Version { get; set; }
+    }
+}
